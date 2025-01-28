c67af2b0ff7d34fa47903763e9e7eb7d6acbc45e
Mr zakir ullah <mrzak802@gmail.com>
2025-01-28 11:55:22 -0800

    comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     <h1 class="text-3xl font-bold underline">
+    <h1 class="text-3xl font-bold ">
     Hello world!
   </h1>
     </div>
